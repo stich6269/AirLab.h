a50ca0ebd4e054c4347446e909f798308de66653
apolischuk <a.polishchuk@mobidev.biz>
2017-03-22 00:05:21 +0200

    - initialize project

---
+++ Debug/DeviceCode.cpp.d
@@ -0,0 +1,36 @@
+C:\Users\Tema\ i\ Tanya\AppData\Local\Temp\VMBuilds\DeviceCode\esp8266_d1\Debug\DeviceCode.cpp.o: \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\Temp\VMBuilds\DeviceCode\esp8266_d1\Debug\DeviceCode.cpp \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/arduino.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/stdlib_noniso.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/binary.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/esp8266_peri.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0/tools/sdk/include/c_types.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/twi.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/Arduino.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/core_esp8266_features.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/pgmspace.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0/tools/sdk/include/ets_sys.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0/tools/sdk/include/c_types.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0/tools/sdk/include/eagle_soc.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0/tools/sdk/include/osapi.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/user_config.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/WCharacter.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/WString.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/pgmspace.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/HardwareSerial.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/Stream.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/Print.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/Printable.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/uart.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/Esp.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/Arduino.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/Updater.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/flash_utils.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/MD5Builder.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/WString.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/Stream.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/md5.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\cores\esp8266/debug.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\variants\d1/pins_arduino.h \
+ C:\Users\Tema\ i\ Tanya\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.3.0\variants\d1/../generic/common.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Enterprise\Common7\IDE\Extensions\pabjyzh5.4hi\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h

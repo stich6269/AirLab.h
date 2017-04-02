fa40a811880a40b83cf9bd60bced06d2c668ffed
polishchuka <a.polishchuk@mobidev.biz>
2017-04-02 10:36:25 +0300

    - add full ads and tgs2602 code sample * current data sample Pressure: 1.01 atm Altitude: -54.78m Temperature: 20.8C Humidity: 45.7% Light: 460 lx Vumeter: 9 VOC lavel: VOC = 3871 CO2Sensor: PPM = 900

--- DeviceCode.ino
+++ DeviceCode.ino
@@ -3,6 +3,7 @@
 #include "SparkFunHTU21D.h"
 #include <BME280I2C.h>
 #include <BH1750.h>
+#include <Adafruit_ADS1015.h>
 
 #define SERIAL_BAUD 115200
 #define I2C_ADDR 0x38
@@ -17,6 +18,8 @@ HTU21D myHumidity;
 BH1750 lightMeter;
 BME280I2C bme;
 
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+
 void setup()
 {
 	Serial.begin(SERIAL_BAUD);
@@ -28,18 +31,21 @@ void setup()
 	myHumidity.begin();
 	lightMeter.begin();
 	bme.begin();
+	adsBegin();
 
 	time = millis();
 }
 
 void loop()
 {
-	if (millis() > time + 5000) {
+	if (millis() > time + 2000) {
 		printBME280Data(&Serial);
 		readHumidity();
 		readLigth();
 		readVu();
+		adsRead();
 
+		Serial.println("CO2Sensor: PPM = " + String(ppm));
 		Serial.println("---------------");
 		time = millis();
 	}
@@ -49,7 +55,29 @@ void loop()
 
 
 
+void adsBegin() {
+	// The ADC input range (or gain) can be changed via the following
+	// functions, but be careful never to exceed VDD +0.3V max, or to
+	// exceed the upper and lower limits if you adjust the input range!
+	// Setting these values incorrectly may destroy your ADC!
+	//                                                                ADS1015  ADS1115
+	//                                                                -------  -------
+	// ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+	// ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+	// ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+	// ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+	// ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+	// ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
 
+	ads.begin();
+}
+
+void adsRead() {
+	int16_t adc0, adc1, adc2, adc3;
+
+	adc0 = ads.readADC_SingleEnded(0);
+	Serial.print("VOC lavel: VOC = "); Serial.println(adc0);
+}
 
 
 void readCo2() {
@@ -70,7 +98,6 @@ void readCo2() {
 			th = l - h;
 			prevVal = myVal;
 			ppm = 5000 * (th - 2) / (th + tl - 4);
-			Serial.println("CO2Sensor: PPM = " + String(ppm));
 		}
 	}
 }
